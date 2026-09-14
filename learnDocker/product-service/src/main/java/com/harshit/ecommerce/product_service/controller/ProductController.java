abc8d1de87a5135f5ab1d25f0d02def8197caa1d
Harshit Chauhan <harshitchauhan471@gmail.com>
2026-09-14 13:18:38 +0530

    Added Docker module

---
+++ learnDocker/product-service/src/main/java/com/harshit/ecommerce/product_service/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.harshit.ecommerce.product_service.controller;
+
+import com.harshit.ecommerce.product_service.entity.Product;
+import com.harshit.ecommerce.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello from Spring Boot";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(@RequestBody Product product){
+        productService.createProduct(product);
+        return "New Product has been created";
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
